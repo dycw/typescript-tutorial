9339be4923a0ce34cf33f21c393ee577ee29d58e
Derek Wan <d.wan@icloud.com>
2023-05-11 21:12:27 +0800

    Finish less 4 (objects & arrays)

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,12 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-character = "luigi";
-age = 40;
-isBlackBelt = true;
-var circ = function (diameter) {
-  return diameter * Math.PI;
-};
-console.log(circ(7.5));
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+var ninja = { name: "mario", belt: "black", age: 30 };
+ninja.age = 40;
+ninja.name = "ryu";
+ninja = { name: "yoshi", belt: "orange", age: 40 };
