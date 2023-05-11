355ac8df95f15559067ae6809c1ce3696ad98217
Derek Wan <d.wan@icloud.com>
2023-05-11 22:08:13 +0800

    Finish lesson 11 (the DOM & type casting)

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+console.log(form.children);
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value);
+  console.log(tofrom.value);
+  console.log(details.value);
+  console.log(amount.valueAsNumber);
+});
