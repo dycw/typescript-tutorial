be79c359a11dd4e259b2cea1f7381338af1bef1d
Derek Wan <d.wan@icloud.com>
2023-05-12 09:51:32 +0800

    Finish lesson 14 (modules)

--- public/app.js
+++ public/app.js
@@ -1,14 +1,4 @@
-"use strict";
-class Invoice {
-  constructor(client, details, amount) {
-    this.client = client;
-    this.details = details;
-    this.amount = amount;
-  }
-  format() {
-    return `${this.client} owes \$${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
