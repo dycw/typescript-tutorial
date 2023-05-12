4b39d36cf2e56435dddd04d9f5fdb5bc8ee15368
Derek Wan <d.wan@icloud.com>
2023-05-12 09:56:33 +0800

    Finish lesson 14 (modules) (#14)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,4 @@
-class Invoice {
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ) {}
-
-  format() {
-    return `${this.client} owes \$${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
