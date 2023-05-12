e173c095d2101d825d08826adf34861bcf569101
Derek Wan <d.wan@icloud.com>
2023-05-12 11:55:55 +0800

    Finish lesson 20 (tuples)

--- src/app.ts
+++ src/app.ts
@@ -18,48 +18,25 @@ const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
   let doc: HasFormatter;
   if (type.value === "invoice") {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
   list.render(doc, type.value, "end");
 });
 
-const addUID = <T extends { name: string }>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return { ...obj, uid };
-};
+let arr = ["ryu", 25, true];
+arr[0] = false;
+arr[1] = "yoshi";
+arr = [30, false, "yoshi"];
 
-let docOne = addUID({ name: "yoshi", age: 40 });
+let tup: [string, number, boolean] = ["ryu", 25, true];
+tup[0] = "ken";
+tup[1] = 30;
 
-console.log(docOne);
-
-enum ResourceType {
-  BOOK,
-  AUTHOR,
-  FILM,
-  DIRECTOR,
-  PERSON,
-}
-
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-}
-
-const docThree: Resource<object> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: { name: "shaun" },
-};
-
-const docFour: Resource<object> = {
-  uid: 2,
-  resourceType: ResourceType.PERSON,
-  data: { name: "yoshi" },
-};
-
-console.log(docThree, docFour);
+let student: [string, number];
+student = ["chun-li", 23];
