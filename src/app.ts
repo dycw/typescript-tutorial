adb70b0843bc9f5b5cd7557df93d966c730de11a
Derek Wan <d.wan@icloud.com>
2023-05-12 10:44:28 +0800

    Finish lesson 18 (generics) (#19)

--- src/app.ts
+++ src/app.ts
@@ -26,3 +26,32 @@ form.addEventListener("submit", (e: Event) => {
   }
   list.render(doc, type.value, "end");
 });
+
+const addUID = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: "yoshi", age: 40 });
+
+console.log(docOne);
+
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: "person",
+  data: { name: "shaun" },
+};
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: "shoppingList",
+  data: ["bread", "milk", "toilet roll"],
+};
+
+console.log(docThree, docFour);
